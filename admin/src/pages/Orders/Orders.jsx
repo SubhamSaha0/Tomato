f504d80ba9115751270d48dfe890eda07c41a4ff
SubhamSaha0 <subhamsaha2511@gmail.com>
2025-05-19 11:37:45 +0530

    updating admin

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -21,7 +21,7 @@ const Orders = ({ url }) => {
 
   const statusHandler = async(event, orderId)=>{
     const response = await axios.post(url+"/api/order/status", {orderId, status: event.target.value})  
-    if(response.success) {
+    if (response.data.success) {
       await fetchAllOrders()
     }  
   }
